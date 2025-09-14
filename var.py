d99a8de219b6d11adc3d3933e7a661ebb46060cd
ghanshyam20 <bhattaraighanshyam274@gmail.com>
2025-09-14 12:28:42 +0300

    pos

---
+++ var.py
@@ -0,0 +1,70 @@
+# # starting from variables to  advance concept 
+
+
+# # adding \n  will print in new line in pytho n programm
+# print("Hello world \nThis is my first program")
+
+
+
+
+#  string concatination in python
+
+
+
+
+# print("my name is Ghanshyam " + "bhattarai")
+
+
+
+
+
+
+# print("Notes from Day 1")
+# print("The print statement is used to output strings")
+# print("Strings are strings of characters")
+# print("String Concatenation is done with the + sign")
+# print("New lines can be created with a \ and the letter n")
+      
+
+
+
+# we used input function to take input from user , store in user variable  and print it 
+# name=input(("what is you name ?"))
+
+
+# print(f"my name is {name}")
+
+
+
+
+print("welcome to the tip calculator")
+bill=float(input("what was the total bill?"))
+
+
+tip=float(input("HOW MUCH YOU WANNA GIVE TIP?"))
+
+p=int(input("how many people to split the bill?"))
+
+
+total_bill_with_tip=bill+(bill*tip/100)
+
+bill_per_person=total_bill_with_tip/p
+
+print(f"EACH PERSON SHOUL PAY :{bill_per_person}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
