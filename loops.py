36c5b21c76721ec95a46477963ed5b309c098826
ghanshyam20 <bhattaraighanshyam274@gmail.com>
2025-09-15 15:55:29 +0300

    random

---
+++ loops.py
@@ -0,0 +1,50 @@
+# we are going to discuss about loops in python 
+
+
+# for loop and while loops
+
+# i will work as temprory variable while looping so that each of the value will assign in i for a moment 
+# fruits=["apple","banana","cherry"]
+
+
+
+# for i in fruits:
+#     print(i)
+
+
+#so we are goin to print the sum for total student score 
+
+
+
+
+scores=[78,65,89,90,45,23,56,78,90]
+# sum=0
+# for i in scores:
+#     sum+=i
+
+# print(f"the total score is {sum}")
+
+
+
+# how should we find the max score from above 
+
+
+# max_score=0
+
+# for i in scores:
+#     if i>max_score:
+#         max_score=i
+
+# print("the max score is ",max_score)
+
+
+# THIS BLOCK CODE WILL HELP YOU TO ADD BETWEEN 1 TO 100 
+# score=0
+# for number in range(1,101):
+#     score+=number
+
+# print(score)
+
+
+
+
