46862152a29677171fd6eb0a0e1c54e9a27afaec
ghanshyam20 <bhattaraighanshyam274@gmail.com>
2025-09-15 12:29:31 +0300

    island

--- var.py
+++ var.py
@@ -37,20 +37,128 @@
 
 
 
-print("welcome to the tip calculator")
-bill=float(input("what was the total bill?"))
+# print("welcome to the tip calculator")
+# bill=float(input("what was the total bill?"))
 
 
-tip=float(input("HOW MUCH YOU WANNA GIVE TIP?"))
+# tip=float(input("HOW MUCH YOU WANNA GIVE TIP?"))
 
-p=int(input("how many people to split the bill?"))
+# p=int(input("how many people to split the bill?"))
 
 
-total_bill_with_tip=bill+(bill*tip/100)
+# total_bill_with_tip=bill+(bill*tip/100)
 
-bill_per_person=total_bill_with_tip/p
+# bill_per_person=total_bill_with_tip/p
 
-print(f"EACH PERSON SHOUL PAY :{bill_per_person}")
+# print(f"EACH PERSON SHOUL PAY :{bill_per_person}")
+
+
+
+#modulo operator 
+
+#it will return the remainder after divison value 
+# a=10%3
+
+# print(a)
+
+
+
+
+
+
+# thsi code is to check whether a number is even or odd
+# num=float(input("Enter a number:"))
+
+
+# if num%2==0:
+#     print("the number is even")
+
+
+
+# else:
+#     print("the number is odd")
+
+
+
+#Nested if/else
+# this is the example of nested if/else condition 
+
+# height=float(input("enter your Height:"))
+
+
+# if height>=120:
+#     print("you can ride the rollercoaster")
+
+#     age=int(input("enter your age:"))
+#     if age<=18:
+#         print("you need to pay 7$")
+
+
+#     else:
+#         print("you need to pay 12$")
+
+
+
+
+# else:
+#     print("you cannot ride rollercoaster")
+
+
+
+
+# PIZZA ORDER PRGRAM
+
+
+print("welcome to pizza delivery")
+
+
+
+size=input("what size of pizza do you want ? (S,M,L:)")
+
+
+pepperoni=input("do you want pepperoni ? (Y/N):")
+
+
+extra_cheese=input("do you want extra cheese?(Y/N):")
+
+
+
+
+bill=0
+
+if size=="S":
+    bill+=15
+
+elif size=="M":
+    bill+=20
+
+elif size=="L":
+    bill+=25
+
+
+else:
+    print("you typed wrong inputs")
+
+
+
+
+if pepperoni=="Y":
+    if size=="S":
+        bill+=2
+
+    else:
+        bill+=3
+
+
+if extra_cheese=="Y":
+    bill+=1
+
+
+
+print(f"your final bill is :{bill}")
+
+
+
 
 
 
